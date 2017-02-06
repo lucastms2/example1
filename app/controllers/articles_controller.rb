28c07478463af6995a3ec03f067805009d4545ca
Lucas <lucastm-@hotmail.com>
2017-02-06 21:04:18 +0000

    Ta indo

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,27 @@
 class ArticlesController < ApplicationController
     
+    def index
+        
+       @articles = Article.all  
+        
+        
+    end
+    
+    
+    
     def new
         
        @article  = Article.new()
         
     end
+    
+    def edit
+        
+       @article = Article.find(params[:id]) 
+        
+        
+    end
+    
     
     def create 
        
@@ -29,6 +46,20 @@ class ArticlesController < ApplicationController
        
         @article = Article.find(params[:id])
     end
+    
+    def update
+        
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was sucessfully update"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
+        
+        
+    end
+    
     
     private
         def article_params
