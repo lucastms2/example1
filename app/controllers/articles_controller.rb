554eda49de12939586bb797f4ae62a118dab30d7
Lucas <lucastm-@hotmail.com>
2017-02-06 19:44:21 +0000

    Fix message

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,8 +10,18 @@ class ArticlesController < ApplicationController
        
        #render plain: params[:article].inspect
        
-       @article = Article.new(article_params)
-       @article.save
+       if @article.save
+           
+           flash[:notice] = "Article was sucessfully created"
+           redirect_to article_path(@article)
+       else
+          render 'new' 
+       end
+    end
+    
+    def show 
+       
+        @article = Article.find(params[:id])
     end
     
     private
