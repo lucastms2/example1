385843aed60a18ca3e88806b4ed0ab67a5b9e95c
Lucas <lucastm-@hotmail.com>
2017-02-06 21:43:23 +0000

    E termineou?

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
     
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
+    
     def index
         
        @articles = Article.all  
@@ -17,7 +19,7 @@ class ArticlesController < ApplicationController
     
     def edit
         
-       @article = Article.find(params[:id]) 
+        
         
         
     end
@@ -43,7 +45,7 @@ class ArticlesController < ApplicationController
     end
     
     def destroy
-       @article = Article.find(params[:id])
+      
        @article.destroy
        
        flash[:notice] = "Article was successfully destroy"
@@ -54,12 +56,12 @@ class ArticlesController < ApplicationController
     
     def show 
        
-        @article = Article.find(params[:id])
+       
     end
     
     def update
         
-        @article = Article.find(params[:id])
+        
         if @article.update(article_params)
             flash[:notice] = "Article was sucessfully update"
             redirect_to article_path(@article)
@@ -72,6 +74,12 @@ class ArticlesController < ApplicationController
     
     
     private
+    
+        def set_article
+            
+            @article = Article.find(params[:id])
+        end
+        
         def article_params
             params.require(:article).permit(:title,:description)
         end
