c4944c834efa480b1e31a01f0b01682294d5c21a
Lucas <lucastm-@hotmail.com>
2017-02-11 20:57:00 +0000

    add assoc

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -30,6 +30,7 @@ class ArticlesController < ApplicationController
      # render plain: params[:article].inspect
        
        @article = Article.new(article_params)
+       @article.user = User.first
        
        if @article.save
            
