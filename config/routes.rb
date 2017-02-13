e983fa8a38bf57a7d58e55151fb089d9edad0d52
Lucas <lucastm-@hotmail.com>
2017-02-13 21:44:45 +0000

    Segunda

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,14 @@ Rails.application.routes.draw do
   get 'about', to: 'welcome#about'
   
   resources :articles
+  
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+  
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
